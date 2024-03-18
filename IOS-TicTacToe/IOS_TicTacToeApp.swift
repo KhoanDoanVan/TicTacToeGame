0b1502d5bef74e93d0e0306e5e8d2b62d570b012
simonisdev <doanvankhoan@example.com>
2024-03-18 19:26:13 +0700

    Initial Commit

---
+++ IOS-TicTacToe/IOS_TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_TicTacToeApp.swift
+//  IOS-TicTacToe
+//
+//  Created by Đoàn Văn Khoan on 18/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct IOS_TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
